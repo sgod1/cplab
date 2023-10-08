006e0b573789060743b12ade319efa929e7b0b66
Simon Godik <simongodik@Simons-MBP.fios-router.home>
2023-10-07 22:48:42 -0700

    copy properties cert files

--- patterns/baw-runtime/prereq/copy-properties.sh
+++ patterns/baw-runtime/prereq/copy-properties.sh
@@ -13,3 +13,33 @@ cp ./cp4ba_LDAP.property $PROPDIR
 ./cp4ba_db_server.property.sh $envfile $PROPDIR/cp4ba_db_server.property
 
 ./cp4ba_user_profile.property.sh $envfile $PROPDIR/cp4ba_user_profile.property
+
+DBCERTDIR=$CERTKUBE/scripts/cp4ba-prerequisites/propertyfile/cert/db
+CERTFILE="db-cert.crt"
+
+mkdir -p $DBCERTDIR/gcddb
+cp $dgcdb_cert $DBCERTDIR/gcddb/$CERTFILE
+
+mkdir -p $DBCERTDIR/bawdocs 
+cp $bawdocs_cert $DBCERTDIR/bawdocs/$CERTFILE
+
+mkdir -p $DBCERTDIR/bawdos 
+cp $bawdos_cert $DBCERTDIR/bawdos/$CERTFILE
+
+mkdir -p $DBCERTDIR/bawtos
+cp $bawtos_cert $DBCERTDIR/bawtos/$CERTFILE
+
+mkdir -p $DBCERTDIR/chosdb 
+cp $chosdb_cert $DBCERTDIR/chosdb/$CERTFILE
+
+mkdir -p $DBCERTDIR/aeosdb 
+cp $aeosdb_cert $DBCERTDIR/aeosdb/$CERTFILE
+
+mkdir -p $DBCERTDIR/icndb
+cp $icndb_cert $DBCERTDIR/icndb/$CERTFILE
+
+mkdir -p $DBCERTDIR/aaedb 
+cp $aaedb_cert $DBCERTDIR/aaedb/$CERTFILE
+
+mkdir -p $DBCERTDIR/bawdb
+cp $bawdb_cert $DBCERTDIR/bawdb/$CERTFILE
