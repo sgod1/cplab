02cc51a11e532383a00f664b9e68ad5bc871d8e3
Simon Godik <simongodik@Simons-MBP.fios-router.home>
2023-10-08 11:33:31 -0700

    psql syntax

--- patterns/baw-runtime/database/pod-create-tablespace.sh
+++ patterns/baw-runtime/database/pod-create-tablespace.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 pod=$1
 
@@ -10,6 +10,9 @@ tsloc="/var/lib/postgresql/data/ts"
 
 cmd1="create tablespace $ts owner $tsu location '$tsloc'"
 cmd2="grant create on tablespace $ts to $tsu"
+
+echo cmd1 => $cmd1
+echo cnd2 => $cnd2
 
 oc exec $pod -- mkdir -p "$tsloc"
 oc exec $pod -- psql -d $db -U postgres -c "$cmd1"
