15926dd7c0b2f048eb7c64272ffd04b82f2c5b4f
Simon Godik <simongodik@Simons-MBP.fios-router.home>
2023-10-09 10:03:48 -0700

    moved cloudpak-operators

---
+++ cloudpak-operators/install-cp4ba-operator.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -x
+
+# namespace parameter
+CP4BA=${1:-"cp4ba"}
+
+echo Target namespace: $CP4BA
+
+# set a project
+oc project $CP4BA
+
+# step 1:
+# create catalog sources
+oc apply -f $OPOLM/catalog_source.yaml
+
+# step 2:
+# install singleton services
+$CP3PT0/setup_singleton.sh --enable-licensing --license-accept
+
+# step 3:
+# create operator group
+cat <<EOF > $OPOLM/cp4ba_operator_group.yaml
+apiVersion: operators.coreos.com/v1alpha2
+kind: OperatorGroup
+metadata:
+  name: ibm-cp4a-operator-catalog-group
+  namespace: $CP4BA
+spec:
+  targetNamespaces:
+  - $CP4BA
+EOF
+
+oc apply -f $OPOLM/cp4ba_operator_group.yaml
+
+# step 5:
+# subscribe to cloud pak operator in one-namespace mode
+CHANNEL="v23.1"
+
+cat <<EOF > $OPOLM/cp4ba_subscription.yaml
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: ibm-cp4a-operator-catalog-subscription
+  namespace: $CP4BA
+spec:
+  channel: $CHANNEL
+  name: ibm-cp4a-operator
+  installPlanApproval: Automatic
+  source: ibm-cp4a-operator-catalog
+  sourceNamespace: openshift-marketplace
+EOF
+
+oc apply -f $OPOLM/cp4ba_subscription.yaml
