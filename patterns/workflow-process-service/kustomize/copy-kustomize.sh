0f1c98f77eced5b2bafb3a62b3bdc0684fb94415
Simon Godik <simon@godik.com>
2023-10-11 09:31:20 -0700

    copy kustomize

--- patterns/workflow-process-service/kustomize/copy-kustomize.sh
+++ patterns/workflow-process-service/kustomize/copy-kustomize.sh
@@ -2,16 +2,24 @@
 
 KUST=$SCRIPTS/kustomize
 
+basedir=`pwd`
+if [[ ! -z $WPS_GIT ]]; then
+    basedir=$WPS_GIT/kustomize
+fi
+
 mkdir -p $KUST
 
-cp -r base $KUST
-cp -r overlay $KUST
+cp -r $basedir/base $KUST
+cp -r $basedir/overlay $KUST
 
 cr="ibm_cp4a_cr_final.yaml"
 
+# copy generated cr to kustomize
 if [[ -f $SCRIPTS/generated-cr/$cr ]]
 then
+    # copy from scripts
     cp $SCRIPTS/generated-cr/$cr $KUST/base
 else
+    # copy from git
     cp $WPS_GIT/generated-cr/$cr $KUST/base
 fi
