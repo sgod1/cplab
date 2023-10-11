3749d99983037d6be94d892598e97937949cdda7
Simon Godik <simon@godik.com>
2023-10-11 08:50:59 -0700

    copy-cr script

--- patterns/workflow-process-service/generated-cr/copy-cr.sh
+++ patterns/workflow-process-service/generated-cr/copy-cr.sh
@@ -4,5 +4,10 @@ cr=${1:-"ibm_cp4a_cr_final.yaml"}
 
 COPYDIR=$SCRIPTS/generated-cr
 
+basedir=`pwd`
+if [[ ! -z $WPS_GIT ]]; then
+    basedir=$WPS_GIT/generated-cr
+fi
+
 mkdir -p $COPYDIR
-cp $cr $COPYDIR
+cp $basedir/$cr $COPYDIR
