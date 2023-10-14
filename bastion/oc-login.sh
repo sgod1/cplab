35c3cb65d1c34b03d07ed7bc27177af84025b01c
Simon Godik <simongodik@Simons-MBP.fios-router.home>
2023-10-14 14:17:36 -0700

    oc-login.sh update

--- bastion/oc-login.sh
+++ bastion/oc-login.sh
@@ -1,5 +1,41 @@
 #!/bin/bash -x
 
-oc login api:6443 --insecure-skip-tls-verify=true --token=`cat auth-token`
+pk=
+t=
+u=
+p=
 
-./set-cloudpak-project.sh $1
+while getopts c:t:u:p: opt
+do
+    case $opt in
+    c) pk="$OPTARG" ;;
+    t) t="$OPTARG" ;;
+    u) u="$OPTARG" ;;
+    p) p="$OPTARG" ;;
+    esac
+done
+
+if test $# -eq 0 -a -f ./auth-token; then
+    # login with saved token
+    oc login api:6443 --insecure-skip-tls-verify=true --token=`cat auth-token`
+
+elif test -z "$pk"; then
+    echo "Usage: oc-login.sh -c project (-t token | -u user -p password)"
+    exit 1
+fi
+
+if test ! -z "$t"; then
+    # login with input token
+    oc login api:6443 --insecure-skip-tls-verify=true --token="$t"
+
+elif test ! -z "$u" -a ! -z "$p"; then
+    # login with username and password
+    oc login api:6443 --insecure-skip-tls-verify=true -u="$u" -p="$p"
+    oc whoami -t > auth-token
+
+else
+    echo "Usage: oc-login.sh -c project (-t token | -u user -p password)"
+    exit 1
+fi
+
+./set-cloudpak-project.sh "$pk"
--- bastion/set-cloudpak-project.sh
+++ bastion/set-cloudpak-project.sh
@@ -1,14 +1,24 @@
 #!/bin/bash -x
 
-cloudpak_project=${1:-"cp4ba1"}
-
 envfile="cloudpak-project.env"
+
+cloudpak_project=$1
+
+if test $# -eq 0 -a -f ./$envfile; then
+    . ./envfile
+    cloudpak_project="$CLOUDPAK_PROJECT"
+fi
+
+if test -z "$cloudpak_project"; then
+    echo "Usage: set-cloudpak-project.sh <cloudpak-project-name>"
+    exit 1
+fi
 
 cat <<EOF > ./$envfile
 #!/bin/bash
-export CLOUDPAK_PROJECT=$cloudpak_project
+export CLOUDPAK_PROJECT="$cloudpak_project"
 EOF
 
 chmod +x ./$envfile
 
-oc project $cloudpak_project
+oc project "$cloudpak_project"
